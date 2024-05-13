7bfa5ecde2c1eca147cc4aa9ac6d911435c5d852
Jessica Oparebea <118492781+JeJay143@users.noreply.github.com>
2024-05-13 04:29:10 +0000

    Add files via upload

---
+++ optics.m
@@ -0,0 +1,15 @@
+clc;
+x = -5:0.1:5;
+y = x;
+l = length(x);
+g = zeros(l);
+for i  = 1:l
+  for j = 1:l
+    g(i,j) = exp(-(x(i)^2 + y(j)^2)/1);
+  end
+end
+figure();
+imagesc(g);
+colormap gray;
+colorbar;
+
